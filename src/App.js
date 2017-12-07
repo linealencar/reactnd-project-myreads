c8a5ae6cec1b726e2f8deeb44346a84d1f425fa0
Aline Alencar <linealencar@gmail.com>
2017-12-06 22:59:55 -0300

    feat: implement book filter by shelf

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import React from 'react';
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI';
 import './App.css';
 import ListBook from './ListBook';
 import SearchBook from './SearchBook';
 import { Route } from 'react-router-dom';
 
 class BooksApp extends React.Component {
-  state = {};
+  state = {
+    books: []
+  };
+
+  componentDidMount() {
+    BooksAPI.getAll().then(books => {
+      this.setState({ books });
+    });
+  }
 
   render() {
     return (
       <div className="app">
         <Route path="/search" component={SearchBook} />
-        <Route exact path="/" component={ListBook} />
+        <Route
+          exact
+          path="/"
+          render={() => <ListBook books={this.state.books} />}
+        />
       </div>
     );
   }
